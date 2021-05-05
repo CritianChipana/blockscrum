c8a98628171aa7431b21bcedfa477c61a0b4a0eb
Jose Navarro Vicente <73919993+Joseenv@users.noreply.github.com>
2021-05-05 15:12:39 -0500

    Update VisionDelProyecto.jsx

--- src/components/block/inicioScreen/VisionDelProyecto.jsx
+++ src/components/block/inicioScreen/VisionDelProyecto.jsx
@@ -12,33 +12,33 @@ export const VisionDelProyecto = () => {
             {/* <h3 className="subtile-visionDeProyecto">VISION DE LA EMPRESA</h3>
             <p className="parrafo-visionDeProyecto">Dar estilos de vida saludables a los clientes</p>
             <h3 className="subtile-visionDeProyecto">MISION DE LA EMPRESA</h3>
-            <p className="parrafo-visionDeProyecto">Mejorar la vida de nuestros clientes, empleados y comunidad, así como inspirar a otros a hacerlo también</p>
-            <h3 className="subtile-visionDeProyecto">ESTUDIO DE MERCADO</h3> */}
+            <p className="parrafo-visionDeProyecto">Mejorar la vida de nuestros clientes, empleados y comunidad, así como inspirar a otros a hacerlo también</p> */}
+            <h3 className="subtile-visionDeProyecto">ESTUDIO DE MERCADO</h3> 
 
             <div className="contenedor-estudio-mercado" >
                 <div>
                     <p>Nombre de la pagina:<h4>Ministerio de Salud - Instituto Nacional de Salud</h4></p>
-                    <p>Pagina: <a href="https://alimentacionsaludable.ins.gob.pe/">click_aqui</a></p>
+                    <p>Pagina: <a href="https://alimentacionsaludable.ins.gob.pe/" target="_BLANK" >click_aqui</a></p>
                     <p>DESCRIPCION: La presente página fue desarrollada con el propósito de brindar información técnica y educativa relacionada a la prevención y tratamiento de enfermedades por fallas alimentarias.</p>
                 </div>
                 <div>
                     <p>Nombre de la pagina:<h4>SEDCA (Sociedad Española de Diatética y Ciencias de la Alimentación.</h4></p>
-                    <p>Pagina: <a href="https://nutricion.org/">click_aqui</a></p>
+                    <p>Pagina: <a href="https://nutricion.org/" target="_BLANK" >click_aqui</a></p>
                     <p>DESCRIPCION: Esta pagina actualmente se dedica a una gran diversidad de tareas relacionadas con diversos campos de la nutrición y la alimentación: a la investigación, a la nutrición comunitaria y divulgación de consejos dietéticos respaldados por la ciencia, a la docencia, y a llevar unos hábitos alimentarios saludables a todos los ámbitos posibles: centros escolares, de la tercera edad, eventos y congresos, empresas, etc.</p>
                 </div>
                 <div>
                     <p>Nombre de la pagina:<h4>vithali - Nutrición Integral</h4></p>
-                    <p>Pagina: <a href="https://vithalinutricion.pe/">click_aqui</a></p>
+                    <p>Pagina: <a href="https://vithalinutricion.pe/" target="_BLANK" >click_aqui</a></p>
                     <p>DESCRIPCION: Esta página ofrece consultas nutricionales personalizadas, analizando composición corporal, estilo de vida, necesidades, para obtener la mejor opción de tratamiento.</p>
                 </div>
                 <div>
                     <p>Nombre de la pagina:<h4>Nutrimed</h4></p>
-                    <p>Pagina: <a href="https://telemednutricionis.wixsite.com/mysite-1">click_aqui</a></p>
+                    <p>Pagina: <a href="https://telemednutricionis.wixsite.com/mysite-1" target="_BLANK" >click_aqui</a></p>
                     <p>DESCRIPCION: Es un página que ofrece el servicio de telemedicina nutricional, para lograr la meta del paciente y bajar de peso de manera segura y eficiente, acompañado de un equipo de profesionales expertos en temas de nutrición. Tales diseñan soluciones prácticas que ayudan al paciente en su bienestar y objetivos. </p>
                 </div>
                 <div>
                     <p>Nombre de la pagina:<h4>Food</h4></p>
-                    <p>Pagina: <a href="https://foood.com/">click_aqui</a></p>
+                    <p>Pagina: <a href="https://foood.com/" target="_BLANK" >click_aqui</a></p>
                     <p>DESCRIPCION: La presente pagina nos muestra una gama variada de platos, el aporte principal de estos platos mostrados en la pagina son la descripcion de cada platillo.</p>
                 </div>
             </div>
